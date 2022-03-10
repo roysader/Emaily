007174717f5d8317e0d7f5108b7125bfa5670cf3
Roy <royisader@gmail.com>
2022-03-10 21:36:10 +0200

    add index.js, add passport and google strategy, add authentication routes, add keys

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
+
 const express = require('express');
+require('./services/passport');
+// const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
 const app = express();
+// mongoose.connect(keys.mongoURI);
 
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy'});
-});
- 
-const PORT = process.env.PORT || 5000;
+require('./routes/authRoutes')(app);
+// instead of 
+//require('./routes/authRoutes')
+//authRoutes(app)
+
+
+const PORT =  5000;
 app.listen(PORT);
+console.log('server is running') 
+ 
+
+
+//ReactApp communicates with express api by exchange http requrest that contain little snippets of jason dataf
+//express interacts iwth mongo db to store records and pull existing records
+//inside mongodb collection contains records
+//collections are people who have signed up which includes name id... which is a jason file, collection of key value pairs(id, name..)
+//each can have its own disticnt charactersitics unlike sql db where each records has the same properties.
+//model class: by using mongoos library we make use of the model class which represents an entire mongodb collection
+//it is used to access a single collection sitting in mongodb
+//which has functions that work with the entire collection like searching or creating collection
+//model instances: are javascript objects that representing a single record in the collection
+//model class related to one collection and instances each represents one record(inside the collection)
+//password: BLQqVexFxwqY9W0h
---
+++ services/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+passport.use
+(
+  new GoogleStrategy
+  (
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, 
+
+    (accessToken, refreshToken, profile, done) => 
+    {
+      console.log('access token', accessToken); 
+      console.log('refresh token', refreshToken);
+      console.log('profile:', profile);
+      //call back function(2nd argument)
+    } //take identifying user info and save it to dabatabase, if we want to.
+  )
+);
