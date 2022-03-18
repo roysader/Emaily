95a30a18a8fca39c7d922c970ceee853ae8d3d44
Roy <royisader@gmail.com>
2022-03-18 13:04:22 +0200

    minor fixes

--- models/User.js
+++ models/User.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose; //same as above
 //mongoose object has a property called "schema", take that prop and asign to new property called "schema"
 
-const userSchema = new Schema({
-  googleId: String
 
+const userSchema = new Schema({
+  googleId: String,
+  sub:String ,
+  // name: String,
+  given_name: String,
+  family_name: String,
+  picture: String,
+  email: String,
+  email_verified: Boolean,
+  locale: String,
 });
 
 mongoose.model('users', userSchema); 
-//by using mongoose.model command, we are telling mongoose that we want to create a new cor
+//by using mongoose.model command, we are telling mongoose that we want to create a new collection by the name of 'user'
+
--- services/passport.js
+++ services/passport.js
@@ -31,17 +31,30 @@ passport.use
       /*console.log("ACCESS TOKEN", accessToken);
       console.log("REFRESH TOKEN", refreshToken);
       console.log("PROFILE", profile);*/
-
+      console.log("PROFILE", profile)
       User.findOne({googleId: profile.id}) //will initiate the query //find one returns a promise, since async
           .then(existingUser=> 
       {
-          if(existingUser){
+          if(!existingUser){
             //we alreayd have a record with the given profile ID
             done(null, existingUser);
           } else {
             //if we dont have  a user record with this id, make a new record
-            new User({ googleId: profile.id}).save()
-            .then(user => done(null, user));
+            new User({
+               googleId: profile.id,
+                sub: profile.sub,
+                // name: profile.name,
+                given_name: profile.given_name,
+                family_name: profile.family_name,
+                picture: profile.family_name,
+                email: profile.email,
+                email_verified: profile.email_verified,
+                locale: profile.locale,
+              }).save()
+            .then(user => {
+              console.log('user after save to db ', user);
+              done(null, user)
+            });
       
             //call back function(2nd argument)
             //take identifying user info and save it to dabatabase, if we want t o.
